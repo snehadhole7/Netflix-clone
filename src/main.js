6293633478644cc885c251d0d6118a43e6749a0d
snehadhole7 <snehadhole7887@gmail.com>
2026-06-05 01:18:41 +0530

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import './styles.css';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
